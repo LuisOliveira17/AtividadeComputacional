ea817b50f891be216e2b4895fd14901e80fc2e48
Luís Gustavo Barbosa Oliveira <157723688+LuisOliveira17@users.noreply.github.com>
2025-04-19 11:22:14 -0300

    Arranjo com repetição

--- aat1/main.c
+++ aat1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-void Permutacao() {
+#include <math.h>
+void Permutacao()
+{
     int numero = 0, numeroExibir = 0;
     int multermos = 1;
 
@@ -9,25 +10,29 @@ void Permutacao() {
 
     numeroExibir = numero;
 
-    if (numero < 0) {
+    if (numero < 0)
+    {
         printf("Numero invalido! Digite um valor maior ou igual a 0.\n");
         return;
     }
 
-    if (numero >= 17) {
+    if (numero >= 17)
+    {
         printf("A maquina apenas executa com exatidao numeros abaixo de 17. Tente novamente.\n");
         return;
     }
 
     // Calcular o fatorial
-    for (int i = 1; i <= numero; i++) {
+    for (int i = 1; i <= numero; i++)
+    {
         multermos *= i;
     }
 
     printf("Fatorial de %d: %d\n\n", numeroExibir, multermos);
 }
 
-void Arranjo() {
+void Arranjo()
+{
     int n, k, numerador = 1, fatorialDenominador = 1;
 
     printf("Digite o numero de elementos (n): ");
@@ -35,31 +40,37 @@ void Arranjo() {
     printf("Digite o numero de posicoes (k) n>=k: ");
     scanf("%d", &k);
 
-    while (n < k || k == 0) {
-        if (n < k) {
+    while (n < k || k == 0)
+    {
+        if (n < k)
+        {
             printf("n deve ser maior ou igual a k. Digite n novamente: ");
             scanf("%d", &n);
         }
-        if (k == 0) {
+        if (k == 0)
+        {
             printf("k deve ser no mínimo 1. Digite k novamente: ");
             scanf("%d", &k);
         }
     }
 
     // Calcula n!
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         numerador *= i;
     }
 
     // Calcula (n - k)!
-    for (int i = 1; i <= (n - k); i++) {
+    for (int i = 1; i <= (n - k); i++)
+    {
         fatorialDenominador *= i;
     }
 
     printf("\nArranjo A(%d,%d): %d\n\n", n, k, numerador / fatorialDenominador);
 }
 
-void Combinacao() {
+void Combinacao()
+{
     int n, k, tempN, tempK; //Elementos e posicoes
     int numerador = 1, denominador = 0, multermoDeno = 1, multermoK = 1;
 
@@ -69,12 +80,15 @@ void Combinacao() {
     printf("Digite o numero de posicoes (k) n>=k:");
     scanf("%d", &k);
 
-    while (n < k || k == 0) {
-        if (n < k) {
+    while (n < k || k == 0)
+    {
+        if (n < k)
+        {
             printf("Digite um valor valido para n: ");
             scanf("%d", &n);
         }
-        if (k == 0) {
+        if (k == 0)
+        {
             printf("Digite um valor valido para k (min. 1): ");
             scanf("%d", &k);
         }
@@ -84,30 +98,35 @@ void Combinacao() {
     tempK = k;
 
     //Calculo do k!
-    while (tempK >= 1) {
+    while (tempK >= 1)
+    {
         multermoK *= tempK;
         tempK--;
     }
 
-    if (n >= k) {
+    if (n >= k)
+    {
         //Calculo Numerador (n)
-        while (n >= 1) {
+        while (n >= 1)
+        {
             numerador *= n;
             n--;
         }
 
         //Calculo do (n-k)!
         denominador = (tempN - k);
-        while (denominador >= 1) {
+        while (denominador >= 1)
+        {
             multermoDeno *= denominador;
             denominador--;
         }
     }
-    printf("\nC(%d,%d)=%d",tempN,k,numerador / (multermoDeno * multermoK));
+    printf("\nC(%d,%d)=%d", tempN, k, numerador / (multermoDeno * multermoK));
     printf("\n\n");
 }
 
-void ArranjoComRepeticao() {
+void ArranjoComRepeticao()
+{
     int n, k;
 
     printf("Digite o numero de elementos(n):");
@@ -115,18 +134,21 @@ void ArranjoComRepeticao() {
     printf("Digite o numero de posicoes (k) k>0:");
     scanf("%d", &k);
 
-    while (k == 0) {
-        if (k == 0) {
+    while (k == 0)
+    {
+        if (k == 0)
+        {
             printf("Digite um valor valido para k (min. 1): ");
             scanf("%d", &k);
         }
     }
-    printf("Arranjo sem repeticao:%d", n^k);
-
+    int resultado = (int)pow(n, k);
+    printf("Arranjo COM repeticao:%d", resultado);
 }
 
-int main(void) {
-    int escolha, escolhaCase1;
+int main(void)
+{
+    int escolha, escolhaCase1, escolhaCase2;
 
     printf("Bem vindo a calculadora de analise combinatoria!\n");
     printf("Menu Principal");
@@ -136,46 +158,62 @@ int main(void) {
     printf(" \nQual a opcao desejada?:");
 
     scanf("%d", &escolha);
-    switch (escolha) {
+    switch (escolha)
+    {
+    case 1:
+        printf("Submenu: Sem Repeticao");
+        printf("   1. Pn");
+        printf("   2. An,k");
+        printf("   3. Cn,k");
+        printf("   4. Voltar ao Menu Principal");
+        printf("\nQual a operacao desejada:");
+        scanf("%d", &escolhaCase1);
+
+        switch (escolhaCase1)
+        {
         case 1:
-            printf("Submenu: Sem Repeticao");
-            printf("   1. Pn");
-            printf("   2. An,k");
-            printf("   3. Cn,k");
-            printf("   4. Voltar ao Menu Principal");
-            printf("\nQual a operacao desejada:");
-            scanf("%d", &escolhaCase1);
-
-            switch (escolhaCase1) {
-                case 1:
-                    Permutacao();
-                    main();
-                    break;
-                case 2:
-                    Arranjo();
-                    main();
-                    break;
-                case 3:
-                    Combinacao();
-                    main();
-                    break;
-                case 4:
-                    main();
-                default:
-                  printf("Escolha uma opcao válida!!\n ");
-                  main();
-            }
-
+            Permutacao();
+            main();
+            break;
         case 2:
-            printf("Submenu: Com Repeticao");
-            printf("   1. PRn1,n2");
-            printf("   2. ARn,k");
-            printf("   3. CRn,k");
-            printf("   4. Voltar ao Menu Principal");
+            Arranjo();
+            main();
+            break;
+        case 3:
+            Combinacao();
+            main();
+            break;
+        case 4:
+            main();
+        default:
+            printf("Escolha uma opcao válida!!\n ");
+            main();
+        }
 
-    case 3:
-      printf("OBRIGADO. VOLTE QUANDO PRECISAR DE MINHA AJUDA");
-      return 0;
+    case 2:
+        printf("Submenu: Com Repeticao\n");
+        printf("   1. PR(n1,n2....,nk)");
+        printf("   2. AR(n,k)");
+        printf("   3. CR(n,k)");
+        printf("   4. VOLTAR PARA O MENU PRINCIPAL");
+        printf("\nQual a operacao desejada:");
+        scanf("%d", &escolhaCase2);
+
+        switch (escolhaCase2)
+        {
+        case 1:
+            ;
+        case 2:
+            ArranjoComRepeticao();
+            break;
+
+
+
+        case 3:
+            printf("OBRIGADO. VOLTE QUANDO PRECISAR DE MINHA AJUDA");
+            return 0;
+        }
+        return 0;
     }
-    return 0;
 }
+
